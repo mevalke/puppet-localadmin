f3a9ace868c8cf5f273aa1a9f45462e2bc60048f
miikka.valkeapaa <miikka1973@gmail.com>
2018-04-02 19:41:13 +0300

    corrections in metadata.json

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
-describe 'puppet_localadmin' do
+describe 'puppet-localadmin' do
   context 'with default values for all parameters' do
-    it { should contain_class('puppet_localadmin') }
+    it { should contain_class('puppet-localadmin') }
   end
 end
